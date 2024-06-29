dd01e5f86608a469a1362c5f1249c61db09c5a89
PogusTheWhisper <gusjangkid@gmail.com>
2024-06-29 14:02:12 +0700

    update api files

--- uphasia_x_scb/src/Routes/Cueing.js
+++ uphasia_x_scb/src/Routes/Cueing.js
@@ -25,28 +25,28 @@ function Cueing() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:8000/guess_word', {
+      const response = await fetch('http://127.0.0.1:12000/gen_question', {
         method: 'POST',
         headers: {
+          'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          occupation,
-          num_of_word: numOfWords,
-          word: '',
+          state: 3,
+          target_word: occupation,
         }),
       });
-
+  
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-
+  
       const result = await response.json();
       setResponseData(result);
     } catch (error) {
       console.error('Error:', error);
     }
-  };
+  };  
 
   return (
     <div className="App">
